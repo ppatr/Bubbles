ef91af97729896f90cc2dc109b894840460fc939
Patrick <puhstp@googlemail.com>
2021-02-18 01:15:43 +0100

    Generelle Umstruckturierung der Bubbles Klasse. Zufalls Positionen der Bubbles. Zufalls Farbe der Bubbles und Bubble Erstellung mit Leertaste.

--- Bubbles.py
+++ Bubbles.py
@@ -1,6 +1,7 @@
-import pygame   #importiert die Pygamme Bibliothek in das Skript   
+import pygame
 import os
-from pygame.constants import (QUIT, KEYDOWN, KEYUP, K_ESCAPE)
+from pygame.constants import (QUIT, KEYDOWN, KEYUP, K_ESCAPE, K_SPACE)
+import random
 
 class Settings(object):
     def __init__(self):
@@ -22,38 +23,58 @@ class Game(object):
         self.pygame.display.set_caption(self.settings.title)
         self.background = self.pygame.image.load(os.path.join(self.settings.images_path, "background.jpg")).convert()
         self.background_rect = self.background.get_rect()
-        self.clock = pygame.time.Clock()
         self.done = False
-#        self.bubble = Bubble(settings)
 
-#        self.all_bubbles = pygame.sprite.Group()
-#        self.all_bubbles.add(self.bubble)
+        self.all_bubbles = pygame.sprite.Group()
 
     def run(self):
         while not self.done:
-            self.clock.tick(self.settings.fps)
             for event in self.pygame.event.get():
                 if event.type == QUIT:
                     self.done = True
                 elif event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         self.done = True
-
+                    if event.key == K_SPACE:
+                        self.addBubble()
             self.draw()
 
     def draw(self):
         self.screen.blit(self.background, self.background_rect)
+        self.all_bubbles.draw(self.screen)
         self.pygame.display.flip()
 
-#class Bubble(pygame.sprite.Sprite):
-#    def __init__(self, settings):
-#        pygame.sprite.Sprite.__init__(self)
-#        self.settings = settings
-#        self.pygame = pygame
-#        self.image = pygame.image.load(os.path.join(self.settings.images_path, "bubble.png")).convert_alpha()
-#        self.image = pygame.transform.scale(self.image, (55, 30))
-#        self.rect = image.get_rect()
+    def addBubble(self):
+        self.all_bubbles.add(Bubble(self.settings, self))
 
+class Bubble(pygame.sprite.Sprite):
+    def __init__(self, settings, game):
+        pygame.sprite.Sprite.__init__(self)
+        self.settings = settings
+        self.pygame = pygame
+
+        self.setSize()
+        self.setImage()
+        self.generateCords()
+
+    def setSize(self):
+        self.width = 10
+        self.height = 10
+
+    def generateCords(self):
+        self.rect.left = random.randint(0, self.settings.width - self.width)
+        self.rect.top = random.randint(0, self.settings.height - self.height)
+
+    def setImage(self):
+        circleFiles = ["redCircle.png", "blueCircle.png", "greenCircle.png", "yellowCircle.png"]
+        self.image = pygame.image.load(os.path.join(self.settings.images_path, circleFiles[random.randint(0, len(circleFiles)) - 1])).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.rect = self.image.get_rect()
+
+
+    def increaseSize(self):
+        self.width += 2
+        self.height += 2
 
 if __name__ == '__main__':
     settings = Settings()

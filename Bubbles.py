7652cb41dbb2d57cc74df76700446d4aec249cb0
Patrick <puhstp@googlemail.com>
2021-04-15 21:45:33 +0200

    Grundspiel implementiert + Bitmaps angepasst

--- Bubbles.py
+++ Bubbles.py
@@ -1,13 +1,13 @@
 import pygame
 import os
 import threading
-from pygame.constants import (QUIT, KEYDOWN, KEYUP, K_ESCAPE, K_SPACE)
+from pygame.constants import (QUIT, KEYDOWN, KEYUP, K_ESCAPE, K_SPACE, MOUSEBUTTONDOWN)
 import random
 
 class Settings(object):
     def __init__(self):
-        self.width = 700
-        self.height = 400
+        self.width = 1280
+        self.height = 720
         self.fps = 60
         self.title = "Bubbles"
         self.file_path = os.path.dirname(os.path.abspath(__file__))
@@ -15,6 +15,29 @@ class Settings(object):
 
     def get_dim(self):
         return (self.width, self.height)
+
+class Mouse(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.pygame = pygame
+        self.settings = settings
+        self.image_orig = pygame.image.load(os.path.join(self.settings.images_path, "crosshair.png")).convert_alpha()
+        self.image = pygame.transform.scale(self.image_orig, (48, 48))
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_clicked = pygame.mouse.get_pressed()
+        if self.rect.collidepoint(*mouse_pos) and mouse_clicked[0]:
+            self.image_orig = pygame.image.load(os.path.join(self.settings.images_path, "crosshair_red.png")).convert_alpha()
+            self.image = pygame.transform.scale(self.image_orig, (48, 48))
+            self.rect = self.image.get_rect()
+        else:
+            self.image_orig = pygame.image.load(os.path.join(self.settings.images_path, "crosshair.png")).convert_alpha()
+            self.image = pygame.transform.scale(self.image_orig, (48, 48))
+            self.rect = self.image.get_rect()
+
+
 
 class Game(object):
     def __init__(self, pygame, settings):
@@ -27,27 +50,34 @@ class Game(object):
         self.done = False
 
         self.all_bubbles = pygame.sprite.Group()
+        
 
     def run(self):
-        counter = 100
-        timer_interval = 10
+        counter = 10
+        timer_interval = 50
         timer_event = pygame.USEREVENT + 1
         pygame.time.set_timer(timer_event , timer_interval)
+        
 
         while not self.done:
+            print (counter)
+            self.all_bubbles.update()
+            mouse.update()
+            mouse.sprite.rect.centerx, mouse.sprite.rect.centery = pygame.mouse.get_pos()
+            pygame.mouse.set_visible(False)
+
             for event in self.pygame.event.get():
                 if event.type == QUIT:
                     self.done = True
                 elif event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         self.done = True
-                    if event.key == K_SPACE:
-                        self.addBubble()
+
 
                 elif event.type == timer_event:
                     counter -= 1
                     if counter == 0:
-                        counter = 100
+                        counter = 10
                         self.addBubble()
                     
             self.draw()
@@ -55,48 +85,67 @@ class Game(object):
     def draw(self):
         self.screen.blit(self.background, self.background_rect)
         self.all_bubbles.draw(self.screen)
+        mouse.draw(self.screen)
         self.pygame.display.flip()
 
     def addBubble(self):
         self.all_bubbles.add(Bubble(self.settings, self))
 
-    def timer(self):
-        pass
 
 class Bubble(pygame.sprite.Sprite):
     def __init__(self, settings, game):
         pygame.sprite.Sprite.__init__(self)
         self.settings = settings
         self.pygame = pygame
+        self.diameter = 5
+        self.time_between_bubble_grow = 25
+        self.time_next_bubble_grow = pygame.time.get_ticks()
 
-        self.setSize()
         self.setImage()
         self.generateCords()
-
-    def setSize(self):
-        self.width = 50
-        self.height = 50
+        self.scale()
 
     def generateCords(self):
-        self.rect.left = random.randint(0, self.settings.width - self.width)
-        self.rect.top = random.randint(0, self.settings.height - self.height)
+        self.rect.left = random.randint(0, self.settings.width - self.diameter)
+        self.rect.top = random.randint(0, self.settings.height - self.diameter)
 
     def setImage(self):
-        circleFiles = ["redCircle.png", "blueCircle.png", "greenCircle.png", "yellowCircle.png"]
-        self.image = pygame.image.load(os.path.join(self.settings.images_path, circleFiles[random.randint(0, len(circleFiles)) - 1])).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.image_orig = pygame.image.load(os.path.join(self.settings.images_path, "Bubble.png")).convert_alpha()
+        self.image = pygame.transform.scale(self.image_orig, (self.diameter, self.diameter))
         self.rect = self.image.get_rect()
 
-    def increaseSize(self):
-        self.width += 2
-        self.height += 2
+    def scale(self):
+        if self.can_grow():
+            self.diameter += random.randint(1, 4)
+            c = self.rect.center
+            self.image = pygame.transform.scale(self.image_orig, (self.diameter, self.diameter))
+            self.rect = self.image.get_rect()
+            self.rect.center = c
 
+            self.time_next_bubble_grow = pygame.time.get_ticks() + self.time_between_bubble_grow
 
-    
+    def update(self):
+        self.scale()
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_clicked = pygame.mouse.get_pressed()
+        if self.rect.collidepoint(*mouse_pos) and mouse_clicked[0]:
+
+            pop = pygame.image.load(os.path.join(self.settings.images_path, "bubblepop.png")).convert_alpha()
+            pop = pygame.transform.scale(pop, (self.diameter, self.diameter))
+            self.image = pop
+            self.rect = self.image.get_rect()
+
+            game.screen.blit(pop, (mouse_pos))
+            self.kill()
+
+    def can_grow(self):
+        return pygame.time.get_ticks() >= self.time_next_bubble_grow
+        
 if __name__ == '__main__':
     settings = Settings()
     pygame.init()
     game = Game(pygame, settings)
+    mouse = pygame.sprite.GroupSingle(Mouse())
     game.run()
 
     pygame.quit()
